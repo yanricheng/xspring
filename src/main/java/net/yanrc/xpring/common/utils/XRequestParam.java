6e15b7e1715b5a7280598aaf18b6163c7c9cf4fd
hzyanricheng <hzyanricheng@corp.netease.com>
2016-04-24 20:04:54 +0800

    mvc

---
+++ src/main/java/net/yanrc/xpring/common/utils/XRequestParam.java
@@ -0,0 +1,88 @@
+package net.yanrc.xpring.common.utils;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ValueConstants;
+
+import java.lang.annotation.*;
+import java.util.Map;
+
+/**
+ * Annotation which indicates that a method parameter should be bound to a web
+ * request parameter.
+ *
+ * <p>Supported for annotated handler methods in Servlet and Portlet environments.
+ *
+ * <p>If the method parameter type is {@link Map} and a request parameter name
+ * is specified, then the request parameter value is converted to a {@link Map}
+ * assuming an appropriate conversion strategy is available.
+ *
+ * <p>If the method parameter is {@link java.util.Map Map&lt;String, String&gt;} or
+ * {@link org.springframework.util.MultiValueMap MultiValueMap&lt;String, String&gt;}
+ * and a parameter name is not specified, then the map parameter is populated
+ * with all request parameter names and values.
+ *
+ * @author Arjen Poutsma
+ * @author Juergen Hoeller
+ * @author Sam Brannen
+ * @since 2.5
+ * @see RequestMapping
+ * @see RequestHeader
+ * @see CookieValue
+ * @see org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
+ * @see org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter
+ * @see org.springframework.web.portlet.mvc.annotation.AnnotationMethodHandlerAdapter
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface XRequestParam {
+
+    /**
+     * Alias for {@link #name}.
+     */
+    @AliasFor("name")
+    String value() default "";
+
+    /**
+     * The name of the request parameter to bind to.
+     * @since 4.2
+     */
+    @AliasFor("value")
+    String name() default "";
+
+    /**
+     * Whether the parameter is required.
+     * <p>Defaults to {@code true}, leading to an exception being thrown
+     * if the parameter is missing in the request. Switch this to
+     * {@code false} if you prefer a {@code null} value if the parameter is
+     * not present in the request.
+     * <p>Alternatively, provide a {@link #defaultValue}, which implicitly
+     * sets this flag to {@code false}.
+     */
+    boolean required() default true;
+
+    /**
+     * The default value to use as a fallback when the request parameter is
+     * not provided or has an empty value.
+     * <p>Supplying a default value implicitly sets {@link #required} to
+     * {@code false}.
+     */
+    String defaultValue() default ValueConstants.DEFAULT_NONE;
+
+    /**
+     * 校验最小长度
+     *
+     * @return
+     */
+    int minLen() default 0;
+
+    /**
+     * 校验最大长度
+     *
+     * @return
+     */
+    int maxLen() default Integer.MAX_VALUE;
+}
--- src/main/java/net/yanrc/xpring/ui/controllor/UserControllor.java
+++ src/main/java/net/yanrc/xpring/ui/controllor/UserControllor.java
@@ -4,11 +4,14 @@ import com.google.common.collect.Sets;
 import net.yanrc.app.common.api.ApiResponse;
 import net.yanrc.app.common.result.DefaultResult;
 import net.yanrc.app.common.result.Result;
+import net.yanrc.app.common.util.JsonUtils;
 import net.yanrc.app.common.util.UUIDGenerator;
 import net.yanrc.xpring.biz.api.UserBizApi;
+import net.yanrc.xpring.common.utils.XRequestParam;
 import net.yanrc.xpring.data.dto.UserDto;
 import net.yanrc.xpring.data.model.User;
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +19,16 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.IdGenerator;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 
 /**
  * UserControllor
@@ -43,28 +44,66 @@ public class UserControllor {
 
     @RequestMapping(value = "/user/save", method = RequestMethod.GET)
     public String saveUserPage(ModelMap resultModel) {
-        resultModel.addAttribute("welcome", "welcome" );
+        resultModel.addAttribute("welcome", "welcome");
         return "user/add";
     }
 
 
-//    Set<MediaType> set = Sets.newHashSet();
+    //    Set<MediaType> set = Sets.newHashSet();
 //    set.add(MediaType.APPLICATION_JSON);
 //    request.setAttribute(HandlerMapping.PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE,set);
 //
-    @RequestMapping(value = "/api/v1/user/save", method = {RequestMethod.GET,RequestMethod.POST})
+    @RequestMapping(value = "/api/v1/user/save", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public ApiResponse saveUser(
             HttpServletRequest request,
-            @RequestParam(required = true,name = "userName",defaultValue = "yanrc") String name) throws InvocationTargetException, IllegalAccessException {
+            @XRequestParam(required = true, name = "userName", defaultValue = "yanrc") String userName
+//            @XRequestParam(required = true, name = "myNme" ) String name,
+//            @XRequestParam(required = true, name = "age", defaultValue = "20") Integer age,
+//            @XRequestParam(required = true, name = "myAge", defaultValue = "30") Integer myAge,
+//            @RequestParam(required = true, name = "xAge", defaultValue = "30") Integer yAge,
+//            @XRequestParam(required = true, name = "married", defaultValue = "true") boolean married,
+//            @XRequestParam(required = true, name = "xmarried", defaultValue = "false") Boolean xmarried,
+//            @XRequestParam(required = true, name = "mymarried", defaultValue = "false") Boolean ymarried,
+//
+//            @RequestParam(required = true, name = "school", defaultValue = "yanrc") ArrayList<String> school,
+//            @XRequestParam(required = true, name = "myschool", defaultValue = "yanrc") ArrayList<String> xschools
+
+    ) throws InvocationTargetException, IllegalAccessException {
         UserDto userDto = new UserDto();
-        userDto.setName(name);
+        userDto.setName(userName);
 
         User user = new User();
-        BeanUtils.copyProperties(user,userDto);
+        BeanUtils.copyProperties(user, userDto);
         user.setId(100);
 
         return new ApiResponse(new DefaultResult<User>(user));
+    }
+
+    static final String key = "c_yanrc";
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResponse voteMzzdyPost(@CookieValue(value = key, required = false) String voteCookie,
+                                     HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println(JsonUtils.fromObject((request.getHeader("User-Agent"))));
+
+        if (StringUtils.isBlank(voteCookie)) {
+            voteCookie = new Date().toString();
+            Cookie c = new Cookie(key, voteCookie);
+            c.setMaxAge(600);
+            response.addCookie(c);
+            System.out.println("无  set cookie:" + voteCookie + "   old:" + voteCookie);
+        } else {
+            System.out.println("有 get cookie:" + voteCookie);
+        }
+
+        for(int i=0;i<10000;i++){
+            System.out.println(i);
+        }
+
+        return new ApiResponse(new DefaultResult<String>(voteCookie));
     }
 
 }
