64ba06d9687419fcf30f9075a49197af0ca30ce0
hzyanricheng <hzyanricheng@corp.netease.com>
2016-11-17 21:26:53 +0800

    搭框架：动态配置

--- web/src/main/java/net/yanrc/xpring/web/filter/AuthFilter.java
+++ web/src/main/java/net/yanrc/xpring/web/filter/AuthFilter.java
@@ -1,8 +1,9 @@
 package net.yanrc.xpring.web.filter;
 
+import net.yanrc.xpring.common.utils.config.ConfigUtils;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -17,14 +18,16 @@ public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-        HttpSession session = req.getSession();
-        System.out.println("sid:" + session.getId());
-        if (session.getAttribute("name") == null) {
-            System.out.println("put name....");
-            session.setAttribute("name", "yanrc");
-        } else {
-            System.out.println("get name:" + session.getAttribute("name").toString());
-        }
+//        HttpSession session = req.getSession();
+//        System.out.println("sid:" + session.getId());
+//        if (session.getAttribute("name") == null) {
+//            System.out.println("put name....");
+//            session.setAttribute("name", "yanrc");
+//        } else {
+//            System.out.println("get name:" + session.getAttribute("name").toString());
+//        }
+
+        System.out.println(ConfigUtils.getDynamicPropertyFactory().getStringProperty("user.name","yanrc").get());
 
         filterChain.doFilter(request, response);
 
