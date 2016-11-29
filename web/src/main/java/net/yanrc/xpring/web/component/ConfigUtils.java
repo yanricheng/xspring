14203401c2d216eedf5c89ebe4eb471d8997fc7a
yanrc <yanrc@servyou.com.cn>
2016-11-29 23:29:08 +0800

    增加动态配置

---
+++ web/src/main/java/net/yanrc/xpring/web/component/ConfigUtils.java
@@ -0,0 +1,33 @@
+package net.yanrc.xpring.web.component;
+
+import com.netflix.config.ConfigurationManager;
+import com.netflix.config.DynamicPropertyFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2016/11/27.
+ */
+public class ConfigUtils {
+    public static void main(String[] args) {
+//        try {
+//            ConfigurationManager.loadCascadedPropertiesFromResources("sample");
+//            String myProp = DynamicPropertyFactory.getInstance().getStringProperty("myprop", "NOT FOUND").get();
+//            System.out.println(myProp);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        try {
+            ConfigurationManager.getDeploymentContext().setDeploymentEnvironment("perf");
+            ConfigurationManager
+                    .loadCascadedPropertiesFromResources("sample");
+            String myProp = DynamicPropertyFactory.getInstance().getStringProperty("xspring.user.name", "NOT FOUND").get();
+            System.out.println(myProp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
