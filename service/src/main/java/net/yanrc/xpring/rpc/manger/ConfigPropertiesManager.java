456eb642224e79b0195297efc26fcf78c0ec70bf
hzyanricheng <hzyanricheng@corp.netease.com>
2016-12-16 20:30:48 +0800

    spring eureka cluster

--- service/src/main/java/net/yanrc/xpring/rpc/manger/ConfigPropertiesManager.java
+++
@@ -1,7 +0,0 @@
-package net.yanrc.xpring.rpc.manger;
-
-/**
- * Created by yanricheng on 16-11-30.
- */
-public interface ConfigPropertiesManager {
-}
--- service/src/main/java/net/yanrc/xpring/rpc/service/impl/ConfigPropertiesServerImpl.java
+++
@@ -1,7 +0,0 @@
-package net.yanrc.xpring.rpc.service.impl;
-
-/**
- * Created by yanricheng on 16-11-30.
- */
-public class ConfigPropertiesServerImpl {
-}
